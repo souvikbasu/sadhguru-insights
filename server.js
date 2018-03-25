f07c6f3704c3d41a1c6ba874ac18bef2531b2540
Souvik Basu <souvikbasu@gmail.com>
2018-03-25 11:59:40 +0530

    fix PORT in server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = 3002;
-console.log(port)
+const port = process.env.PORT || 3001;
 app.set('port', port);
 
 const server = http.createServer(app);
