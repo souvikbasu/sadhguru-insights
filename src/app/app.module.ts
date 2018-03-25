9bd60129f7d890d8a2c67cebbf445aebbe288663
Souvik Basu <souvikbasu@gmail.com>
2018-03-25 12:41:54 +0530

    add home component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatSelectModule, MatInputModule, MatToolbarModule, MatCardModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from '../components/home/home.component';
+import { appRoutes } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      {}
+    ),
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatSelectModule, MatInputModule, MatToolbarModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import {Routes} from '@angular/router';
+
+import { HomeComponent } from '../components/home/home.component';
+
+export const appRoutes: Routes = [
+  {path: '', component: HomeComponent}
+];
