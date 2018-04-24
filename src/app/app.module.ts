97935d9943a7f9075940a5be91a1569bf54d05b2
Ankit <ankit@qexon.com>
2018-04-24 18:36:14 +0530

    create separate folder for component and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { MatButtonModule, MatSelectModule, MatInputModule, MatToolbarModule, MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AddvideoComponent } from './addvideo/addvideo.component';
+import { HomeComponent } from '../components/home/home.component';
+import { AddvideoComponent } from '../components/addvideo/addvideo.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { VideoInfoService } from './video-info.service';
+import { VideoInfoService } from '../services/video-info.service';
 import { HttpModule } from '@angular/http';
 
 @NgModule({
