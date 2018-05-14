d6818a550d830993ee3d2467c23e55b9096f7111
Basu, Souvik <Souvik.Basu@tesco.com>
2018-05-14 09:46:53 +0530

    fix tslint error in home component

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -21,35 +21,35 @@ export class HomeComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
-  bindVideos(){
-    this.videoInfoService.getTags().subscribe( res =>{
+  bindVideos() {
+    this.videoInfoService.getTags().subscribe( res => {
       let dataArr = res.map(function(data) {
         return data['tags'];
-      })
-      var mergedArr = [].concat.apply([], dataArr);      
-      let uniqueArray = mergedArr.filter(function (item, pos) {
+      });
+      let mergedArr = [].concat.apply([], dataArr);
+      const uniqueArray = mergedArr.filter(function (item, pos) {
         return mergedArr.indexOf(item) == pos;
-      })
+      });
       this.buttonArr = uniqueArray;
       setTimeout(() => {
         this.getVideo(uniqueArray[0], 0);
       }, 1000);
-      
-    })
+
+    });
   }
 
-  getVideo(value,index){
+  getVideo(value, index){
     this.addCssButtons(document.getElementsByClassName('myHomeBtn'));
-    var btn = document.getElementsByClassName('myHomeBtn')[index];
+    const btn = document.getElementsByClassName('myHomeBtn')[index];
     btn['style']['background'] = '#af87f7';
     this.videoInfoService.getVideo(value).subscribe( res => {
       this.dataArr = res;
-    })
+    });
   }
 
   addCssButtons(allBtn){
-    for(var i = 0; i < allBtn.length; i++){
-      var btn = allBtn[i];
+    for (let i = 0; i < allBtn.length; i++){
+      const btn = allBtn[i];
       btn['style']['background'] = '#673ab7';
     }
   }
