a748d5de00621a98a39fb2e8d99aeeb9499d241b
Basu, Souvik <Souvik.Basu@tesco.com>
2018-05-14 09:36:44 +0530

    linting cideo service file

--- src/services/video-info.service.ts
+++ src/services/video-info.service.ts
@@ -12,23 +12,23 @@ export class VideoInfoService {
 
   saveVideo(value) {
     this.headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.post(this.apiUrl+'/video', value, { headers: this.headers })
+    return this.http.post(this.apiUrl + '/video', value, { headers: this.headers })
       .map(res => res.json()
     );
   }
-  
+
   getTags() {
     this.headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.get(this.apiUrl+'/tags', { headers: this.headers })
+    return this.http.get(this.apiUrl + '/tags', { headers: this.headers })
       .map(res => res.json()
       );
   }
 
   getVideo(value) {
     this.headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.get(this.apiUrl + '/videos?tag='+value, { headers: this.headers })
+    return this.http.get(this.apiUrl + '/videos?tag=' + value, { headers: this.headers })
       .map(res => res.json()
       );
-      
+
   }
 }
