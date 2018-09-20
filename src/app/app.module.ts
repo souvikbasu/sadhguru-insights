dcab7b528b3ab30392ad5c5d8656be5022b7ffa0
Souvik Basu <souvikbasu@gmail.com>
2018-09-20 17:32:49 +0530

    set pagination size to 40 items per page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { MatButtonModule, MatSelectModule, MatInputModule, MatToolbarModule, MatCardModule, MatTabsModule, MatTableModule } from '@angular/material';
-import { AppComponent } from './app.component';
-import { HomeComponent } from '../components/home/home.component';
-import { AddvideoComponent } from '../components/addvideo/addvideo.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { VideoInfoService } from '../services/video-info.service';
-import { HttpModule } from '@angular/http';
-import { ViewVideoComponent } from '../components/view-video/view-video.component';
-import { VideosComponent } from '../components/videos/videos.component';
-import {NgxPaginationModule} from 'ngx-pagination';
+import { AppRoutingModule } from "./app-routing.module";
+import {
+  MatButtonModule,
+  MatSelectModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatTabsModule,
+  MatTableModule
+} from "@angular/material";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "../components/home/home.component";
+import { AddvideoComponent } from "../components/addvideo/addvideo.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { VideoInfoService } from "../services/video-info.service";
+import { HttpModule } from "@angular/http";
+import { ViewVideoComponent } from "../components/view-video/view-video.component";
+import { VideosComponent } from "../components/videos/videos.component";
+import { NgxPaginationModule } from "ngx-pagination";
 
 @NgModule({
   declarations: [
@@ -23,12 +31,22 @@ import {NgxPaginationModule} from 'ngx-pagination';
     VideosComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, HttpModule,
-    AppRoutingModule, ReactiveFormsModule, FormsModule,
-    MatButtonModule, MatSelectModule, MatInputModule, MatToolbarModule, MatCardModule, MatTabsModule,
-    MatTableModule, NgxPaginationModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatTableModule,
+    NgxPaginationModule
   ],
   providers: [VideoInfoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
